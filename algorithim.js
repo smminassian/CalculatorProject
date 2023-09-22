74c058119779c17ca1031b2a0b59158365e27bb5
smminassian <sebastienminassian203@gmail.com>
2023-09-22 16:35:19 -0700

    added a lot of functions
    I might need to get rid of the first function and change it to a class.

--- algorithim.js
+++ algorithim.js
@@ -1,34 +1,57 @@
 //Problem: I need to get my calculator to work.
-//SmallerProblem: I need to set up my class and set up the functions I will use
-//I need a clear function to clear all data 
-
-class calculator{
-    private:
-    double prevNumber;
-    double currentNumber;
-
-    public:
-    Clear(prevnumber, currentNumber){
-        //sets prevnumber, currentNumber to empty string
-
+//SmallerProblem: I need to set up my class and set up the functions I will use(Get it check by professor or tutor)
+//I need a clear function to clear all data(Completed)
+'use strict';
+    
+    const calculator = function(prevNumber, currentNumber){
+        this.prevNumber = prevNumber;
+        this.currentNumber = currentNumber;
+        prevNumber = 0.0;
+        currentNumber = 0.0;
+        clear;
     }
 
-    append(){
-        //append to the end of a vector or an array
-        while(let x != '='){
-            //vector.pushback(x)
-            if(x == '='){
-                break;
-            }
-        }
-        
-        
+    const clear = function(prevNumber, currentNumber){
+        prevNumber = 0.0;
+        currentNumber = 0.0;
     }
 
-    calculate(prevnumber, currentNumber){
+    const append = function(value){
+        const value = 0;
+        currentNumber = currentNumber + value;
+        return currentNumber;
+    }
 
+
+    const calculate = function(prevNumber, currentNumber){
         //create a switch case that goes over +, -, x, /, and division by zero error
+        let operation = '+' || '-' || '*' || '/' || '--' || '++' || '**' || '//';
+        switch(operation){
+            case('+'):
+                return prevNumber + currentNumber;
+            break;
+            case('-'):
+                return prevNumber + currentNumber;
+            break;
+            case('*'):
+                return prevNumber * currentNumber;
+            break;
+            case('/'):
+                let calculation = prevNumber / currentNumber;
+                if(calculation == 0){
+                    throw("Error");
+                }
+            break;
+            case('--'):
+            break;
+            case('++'):
+            break;
+            case('**'):
+            break;
+            case('//'):
+            break;
+        }
     }
+
 
     
-}
