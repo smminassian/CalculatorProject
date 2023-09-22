9fdc59204400808c44932600b42bf73900ec04b3
smminassian <sebastienminassian203@gmail.com>
2023-09-21 22:45:05 -0700

    wrote pseudo code in C++

--- algorithim.js
+++ algorithim.js
@@ -1,8 +1,25 @@
-class Calculator{
-    private double prevElement;
-    var currentElement;
-    Calculator(){
-        this.prevElement = prevElement;
-        this.currentElement = currentElement;
+//Problem: I need to get my calculator to work.
+//SmallerProblem: I need to set up my class and set up the functions I will use
+//I need a clear function to clear all data 
+
+class calculator{
+    private double prevNumber;
+    double currentNumber;
+
+    public:
+    Clear(prevnumber, currentNumber){
+        //sets prevnumber, currentNumber to empty string
+
     }
+
+    append(){
+        //append to the end of a vector or an array
+        
+    }
+
+    calculate(){
+        //create a switch case that goes over +, -, x, /, and division by zero error
+    }
+
+    
 }
