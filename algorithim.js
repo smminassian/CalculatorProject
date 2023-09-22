808255c6ca726fdc1888b362a863d5a33a7e7aef
smminassian <sebastienminassian203@gmail.com>
2023-09-22 16:37:58 -0700

    added  a class, I dont know why it does not like const

--- algorithim.js
+++ algorithim.js
@@ -2,8 +2,8 @@
 //SmallerProblem: I need to set up my class and set up the functions I will use(Get it check by professor or tutor)
 //I need a clear function to clear all data(Completed)
 'use strict';
-    
-    const calculator = function(prevNumber, currentNumber){
+    class calculator{
+        calculator = function(prevNumber, currentNumber){
         this.prevNumber = prevNumber;
         this.currentNumber = currentNumber;
         prevNumber = 0.0;
@@ -11,19 +11,19 @@
         clear;
     }
 
-    const clear = function(prevNumber, currentNumber){
+    clear = function(prevNumber, currentNumber){
         prevNumber = 0.0;
         currentNumber = 0.0;
     }
 
-    const append = function(value){
+    append = function(value){
         const value = 0;
         currentNumber = currentNumber + value;
         return currentNumber;
     }
 
 
-    const calculate = function(prevNumber, currentNumber){
+    calculate = function(prevNumber, currentNumber){
         //create a switch case that goes over +, -, x, /, and division by zero error
         let operation = '+' || '-' || '*' || '/' || '--' || '++' || '**' || '//';
         switch(operation){
@@ -52,6 +52,7 @@
             break;
         }
     }
-
+    
+    }
 
     
