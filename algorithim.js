0bedd4e76a3ed87c2027c3353d2db73a2507adf5
smminassian <sebastienminassian203@gmail.com>
2023-09-27 12:43:25 -0700

    adding events

--- algorithim.js
+++ algorithim.js
@@ -8,13 +8,15 @@
     }
 
     clear(){
-        this.prevNumber = 0.0;
-        this.currentNumber = 0.0;
+        this.prevNumber = '';
+        this.currentNumber = '';
+        display();
     }
 
     append(value){
         this.prevNumber = this.currentNumber
         this.currentNumber = value;
+        display();
         return this.currentNumber; 
     }
 
@@ -52,22 +54,34 @@
 }
     }
     const calc = new calculator();
-
-    // const number = document.querySelectorAll(".number");
+     // const number = document.querySelectorAll(".number");
     // number.addEventListener("number").forEach(number => {number.addEventListener("click", calc.append(value))});
     // const Operation = document.querySelectorAll(".Operation");
     // Operation.addEventListener("Operation").forEach(Operation => {Operation.addEventListener("click", calc.append(value))});
     // const clear = document.querySelector(".clear")
     // clear.addEventListener("clear").forEach(clear => {clear.addEventListener("click", calc.clear())});
     //Tomorrow we wil change operation, clear, and number to id and we will figure out how to get our buttons to work. We will use get element by ID
-    function myAppendFunction(value){
-        if(value != NaN){
-            calc.append(value);
-        }
+    function Operation(){
+        if()
+        
     }
 
-    calc.append(2); 
-    calc.append(4);
-    console.log(calc.calculate('+')); 
+    function setClear(){
+        calc.clear();
+    }
+
+    function myAppendFunction(value){
+       if(value != NaN){
+        calc.append(value);
+       }
+    }
+
+    function display(){
+        const input = document.getElementById('inputBox');
+        if(value != NaN){
+            input.value = calc.currentNumber
+        }
+    }
+    
     
     
