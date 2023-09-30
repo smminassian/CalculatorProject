d0e041a831cc18fdc964c216aa04a1c5b0ab6cb7
smminassian <sebastienminassian203@gmail.com>
2023-09-29 18:32:23 -0700

    fixed stuff

--- algorithim.js
+++ algorithim.js
@@ -1,6 +1,179 @@
+// 'use strict';
+
+//     class calculator{
+//         constructor(){
+//         this.prevNumber = '';
+//         this.currentNumber = '';
+//     }
+
+//     clear(){
+//         this.prevNumber = '';
+//         this.currentNumber = '';
+//     }
+
+//     append(value){
+//         this.prevNumber = this.currentNumber.toString();
+//         this.currentNumber = value.toString();
+//         display(this.currentNumber);
+//         return this.currentNumber;
+//     }
+
+//     calculate(operation){
+//          if(operation === '+'){
+//             return Number(this.prevNumber) + Number(this.currentNumber);
+//         }
+//         else if(operation === '-'){
+//             return Number(this.prevNumber) - Number(this.currentNumber);
+//         }
+//         else if(operation === '*'){
+//             return Number(this.prevNumber) * Number(this.currentNumber);
+//         }
+//         else if(operation === '/'){
+//             if((Number(this.currentNumber)) != 0){
+//                 return Number(this.prevNumber) / Number(this.currentNumber);
+//            }
+//            else{
+//             console.log("NaN");
+//            }
+//         }
+//     }
+// }
+    
+//     const calc = new calculator();
+
+    
+//     const numberButton = document.querySelectorAll(".number");
+//     const OperationButton = document.querySelectorAll(".Operation");
+
+
+//      //OperationButton.forEach(button => {button.addEventListener("click", Operation(button.value))}); 
+
+
+//      function Operation(value){
+//         if(value === '+' || '-' || '*' || '/'){  
+//             const result = calc.calculate(value);
+//             display(result);
+//                 if(value === '='){
+//      }
+//     }
+// }
+    
+//     numberButton.forEach(button => {button.addEventListener("click", myAppendFunction(button.value))});
+                                                                                   
+//     function myAppendFunction(value){
+//        if(!isNaN(value)){
+//         calc.append(value);
+//         display(value);
+//        }
+
+//     }
+
+//     function setClear(){
+//         calc.clear();
+//         display(calc.currentNumber);
+//     }
+
+//     function display(value){
+//         const input = document.getElementById('inputBox');
+//         if(value != NaN){
+//             input.value = value;
+//         }
+//      }
+    
+// 'use strict';
+
+//     class calculator{
+      
+//         constructor(){
+//         this.prevNumber = '';
+//         this.currentNumber = '';
+//     }
+
+//     clear(){
+//         this.prevNumber = '';
+//         this.currentNumber = '';
+//     }
+      
+//     append(value){
+//         this.prevNumber = this.currentNumber.toString();
+//         this.currentNumber = value.toString();
+//         display(this.currentNumber); 
+//         return this.currentNumber;
+//     }
+
+//     calculate(operation){
+//          if(operation === '+'){
+//             return Number(this.prevNumber) + Number(this.currentNumber);
+//         }
+//         else if(operation === '-'){
+//             return Number(this.prevNumber) - Number(this.currentNumber);
+//         }
+//         else if(operation === '*'){
+//             return Number(this.prevNumber) * Number(this.currentNumber);
+//         }
+//         else if(operation === '/'){
+//             if((Number(this.currentNumber)) != 0){
+//                 return Number(this.prevNumber) / Number(this.currentNumber);
+//            }
+//            else{
+//             console.log("NaN");
+//            }
+//         }
+//     }
+// }
+    
+
+//      function Operation(val){
+//         if(val === '+' || '-' || '*' || '/'){  
+//             const result = calc.calculate(val);
+//           		display(result);
+//                 if(val === '='){
+//                     display(result);
+//                 }
+//     	 }
+//     }
+    
+                                                                                   
+//     function myAppendFunction(val){
+//        if(!isNaN(val)){
+//         calc.append(val);
+//         display(val);
+//        }
+
+//     }
+
+//     function setClear(){
+//         calc.clear();
+//         display(calc.currentNumber);
+//     }
+
+//     function display(value){
+//         const input = document.getElementById('inputBox');
+//         if(value != NaN){
+//             input.value = value;
+//         }
+//      }
+
+
+//     const calc = new calculator();
+    
+//     const numberButton = document.querySelectorAll(".number");
+//     const OperationButton = document.querySelectorAll(".Operation");
+
+//     OperationButton.forEach(button => {button.addEventListener("click", Operation(button.value))}); 
+    
+//     //numberButton.forEach(button => {button.addEventListener("click", myAppendFunction(button.value))});
+
+	
+// 	for(var i =0; i<numberButton.length; i++)
+//     {
+//      	numberButton[i].addEventListener("click", myAppendFunction(numberButton[i].value));
+//     }
+
 'use strict';
 
     class calculator{
+      
         constructor(){
         this.prevNumber = '';
         this.currentNumber = '';
@@ -10,11 +183,11 @@
         this.prevNumber = '';
         this.currentNumber = '';
     }
-
+      
     append(value){
         this.prevNumber = this.currentNumber.toString();
         this.currentNumber = value.toString();
-        display(this.currentNumber);
+        display(this.currentNumber); 
         return this.currentNumber;
     }
 
@@ -38,30 +211,23 @@
         }
     }
 }
+const calc = new calculator();
     
-    const calc = new calculator();
 
-    
-    const numberButton = document.querySelectorAll(".number");
-    const OperationButton = document.querySelectorAll(".Operation");
-
-
-     OperationButton.addEventListener(".Operation").forEach(button => {button.addEventListener("click", Operation(val))}); 
-
-
-     function Operation(val){
+     function Operation(event){
+        let val = event.target.value;
         if(val === '+' || '-' || '*' || '/'){  
             const result = calc.calculate(val);
-            display(result);
+          		display(result);
                 if(val === '='){
                     display(result);
                 }
-     }
+    	 }
     }
     
-    numberButton.addEventListener(".number").forEach(button => {button.addEventListener("click", myAppendFunction(val))});
                                                                                    
-    function myAppendFunction(val){
+    function myAppendFunction(event){
+        let val = event.target.value;
        if(!isNaN(val)){
         calc.append(val);
         display(val);
@@ -80,4 +246,34 @@
             input.value = value;
         }
      }
+
+
+ window.addEventListener('DOMContentLoaded', (event) => {
+
+   
+   	
     
+    const numberButton = document.querySelectorAll(".number");
+    const OperationButton = document.querySelectorAll(".Operation");
+
+    OperationButton.forEach(button => {button.addEventListener("click", event => {Operation(event)},false); 
+    
+    //numberButton.forEach(button => {button.addEventListener("click", myAppendFunction(button.value))});
+
+	
+	for(var i =0; i<numberButton.length; i++)
+    {
+     	numberButton[i].addEventListener("click", event => {myAppendFunction(event)},false);
+    }
+    document.getElementById("clear").addEventListener("click", event=> {setClear()}, false);
+
+   
+ });
+    
+
+ });
+
+
+
+
+
